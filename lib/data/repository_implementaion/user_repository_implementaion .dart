3686e4def1d6107a8deabf1a0313f0d92ed1040d
Amir0710 <Rabbaniamir44@gmail.com>
2025-07-12 17:01:26 +0330

    implementation

---
+++ lib/data/repository_implementaion/user_repository_implementaion .dart
@@ -0,0 +1,36 @@
+import 'package:baladeston/domain/entities/user.dart';
+import 'package:baladeston/domain/models/user_query_filter.dart';
+import 'package:baladeston/domain/repository/user_repository.dart';
+
+class UserRepositoryImpleentaion implements UserRepository{
+  @override
+  Future<void> changePassword({required String newPassword}) {
+    // TODO: implement changePassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteUser() {
+    // TODO: implement deleteUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<User>> getUsers({required UserQueryFilter filter}) {
+    // TODO: implement getUsers
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateUser({required User user}) {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
+    
+}
